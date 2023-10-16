5c0b6f134181081b023fe0c6b23ed7a27afe1ec4
AppSiDaL <giliecrack343@gmail.com>
2023-10-16 12:30:25 -0600

    fixed build steeps

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 /* global process */
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 
 app.use(express.static('dist'))
 
